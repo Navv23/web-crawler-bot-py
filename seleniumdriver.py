87136dbf5bed3342732314eb4950635c9b6d648f
Navv23 <navaneethan.ghanti@gmail.com>
2024-05-11 23:49:53 +0530

    changed webdriver logic

--- seleniumdriver.py
+++ seleniumdriver.py
@@ -1,22 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SeleniumDriver:
-    def __init__(self, headless_mode, user_agent=None):
+    def __init__(self, headless_mode=True, user_agent=None, webdriver_path=None):
         self.headless_mode = headless_mode
         self.user_agent = user_agent
-        
-    def scrape(self, url):
-        options = Options()
-        if self.headless_mode:
-            options.add_argument("--headless")
-        if self.user_agent:
-            options.add_argument(f"user-agent={self.user_agent}")
+        self.webdriver_path = webdriver_path
+        self.driver = None
 
-        driver = webdriver.Chrome(options=options)
+    def get_driver(self):
         try:
-            driver.get(url)
-            content = driver.page_source
-            return content
-        finally:
-            driver.quit()
+            options = Options()
+            if self.headless_mode:
+                options.add_argument("--headless")
+            if self.user_agent:
+                options.add_argument(f"user-agent={self.user_agent}")
+
+            if self.webdriver_path:
+                self.driver = webdriver.Chrome(executable_path=self.webdriver_path, options=options)
+            else:
+                self.driver = webdriver.Chrome(options=options)
+
+            return self.driver
+        except Exception as e:
+            print(f"Error initializing WebDriver: {e}")
+            return None
--- webcrawler.py
+++ webcrawler.py
@@ -1,32 +1,34 @@
 import logging
 import requests
-from requests.adapters import HTTPAdapter
 import time
 import random
 import os
 
-from urllib3 import Retry
 from seleniumdriver import SeleniumDriver
 from requestmanager import RequestManager
+from bs4 import BeautifulSoup
 
 request_manager = RequestManager()
 
-class WebCrawler:
-    def __init__(self, url, client, use_session=False, min_delay=1, max_delay=3):
-        self.url = url
+class WebCrawler(SeleniumDriver):
+    def __init__(self, client, webdriver_path=None, use_session=False, min_delay=1, max_delay=3):
         self.client = client
+        self.webdriver_path = webdriver_path
         self.use_session = use_session
         self.min_delay = min_delay
         self.max_delay = max_delay
         self.requests_count = 4 
 
         self.logger = self.__configure_logger()
-
+        
         if self.use_session:
             self.__create_session()
         else:
             self.session = None
 
+        if self.client == 'selenium':
+            self.driver = SeleniumDriver(headless_mode=False).get_driver()
+        
     def __configure_logger(self):
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
@@ -41,25 +43,25 @@ class WebCrawler:
         logger.addHandler(fh)
 
         return logger
-
-    def scrape_page(self):
+    
+    def scrape(self, url):
         try:
             if self.client == 'requests':
-                content = self.__scrape_with_requests()
+                content = self.__scrape_with_requests(url)
             elif self.client == 'selenium':
-                content = self.__scrape_with_selenium()
+                content = self.__scrape_with_selenium(url)
             else:
                 raise ValueError("Unsupported scraping client")
             
-            self.logger.info(f"Scraping completed for URL: {self.url}")
+            self.logger.info(f"Scraping completed")
             return content
         except Exception as e:
             self.logger.error(f"Error occurred while scraping: {e}")
             raise
 
-    def __scrape_with_requests(self):
+    def __scrape_with_requests(self, url):
         session_used = "Using session" if self.use_session else "Not using session"
-        
+    
         try:
             delay = random.uniform(self.min_delay, self.max_delay)
             time.sleep(delay)
@@ -75,69 +77,41 @@ class WebCrawler:
             
             if self.use_session:
                 session = self.session
-                response = session.get(self.url, headers=headers)
+                response = session.get(url, headers=headers)
             else:
-                response = requests.get(self.url, headers=headers)
+                response = requests.get(url, headers=headers)
             
             if response and response.status_code == 200:
-                self.logger.info(f"{session_used} - Successful request, Request made to {self.url}, Response code: {response.status_code}")
+                self.logger.info(f"{session_used} - Successful request, Request made to {url}, Response code: {response.status_code}")
                 if isinstance(response.text, str):
                     return response.text
                 else:
-                    error_message = f"Invalid Response: Response for the URL {self.url} is not a string"
+                    error_message = f"Invalid Response: Response for the URL {url} is not a string"
                     self.logger.error(error_message)
                     return error_message
             else:
                 if response:
-                    self.logger.warning(f"{session_used} - For the {self.url}, request was not successful. Response code: {response.status_code}")
+                    self.logger.warning(f"{session_used} - For the {url}, request was not successful. Response code: {response.status_code}")
                 else:
-                    self.logger.warning(f"{session_used} - For the {self.url}, request was not successful. No response received.")
+                    self.logger.warning(f"{session_used} - For the {url}, request was not successful. No response received.")
         except requests.RequestException as e:
             self.logger.error(f"Error occurred during request: {e}")
             raise
 
-    def __scrape_with_selenium(self):
+
+    def __scrape_with_selenium(self, url):
         try:
             delay = random.uniform(self.min_delay, self.max_delay)
             time.sleep(delay)
-
-            user_agent = request_manager.rotate_user_agent()
-
-            scrapper = SeleniumDriver(headless_mode=True, user_agent=user_agent)
             
-            content = scrapper.scrape(self.url)
+            self.driver.get(url)
 
-            if isinstance(content, str):
-                self.logger.info(f"Selenium: Successful request for the url: {self.url}")
-                return content
-            else:
-                error_message = "Invalid Response: Response is not a string"
-                self.logger.error(error_message)
-                return error_message
+            page = BeautifulSoup(self.driver.page_source, 'html.parser')
+
+            self.logger.info(f"Selenium: Successful request for the URL: {url}")
+            return page
+
         except Exception as e:
             self.logger.error(f"Error occurred while scraping with Selenium: {e}")
             raise
 
-    def __create_session(self):
-        self.session = requests.Session()
-        retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504, 429])
-        self.session.mount('http://', HTTPAdapter(max_retries=retries))
-        self.session.mount('https://', HTTPAdapter(max_retries=retries))
-
-# Example usage for Selenium:
-# url = "https://www.youtube.com/watch?v=qCuPvl1kIro"
-# scrapper = WebCrawler(url, 'selenium')
-# try:
-#     content = scrapper.scrape_page()
-#     print(content)
-# except Exception as e:
-#     print(e)
-
-# Example usage for Requests:
-# url = "https://www.youtube.com/watch?v=qCuPvl1kIro"
-# scrapper = WebCrawler(url, 'requests', use_session=False)
-# try:
-#     content = scrapper.scrape_page()
-#     print(content)
-# except Exception as e:
-#     print(e)
